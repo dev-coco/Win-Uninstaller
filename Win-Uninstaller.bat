e33d418774de7dd23b4e3dab411f4232ac80b183
dev-coco <63853884+dev-coco@users.noreply.github.com>
2020-04-23 15:51:16 -0400

    Update Win-Uninstaller.bat

--- Win-Uninstaller.bat
+++ Win-Uninstaller.bat
@@ -1,7 +1,6 @@
-@ echo off
-curl -s https://raw.githubusercontent.com/dev-coco/Win-Uninstaller/master/UninstallForWIN.bat -o C:\Users\%username%\Desktop\UninstallForWIN.bat
-start "" C:\Users\%username%\Desktop\UninstallForWIN.bat
-ping 127.0.0.1 -n 10
+@echo off
+curl -s https://raw.githubusercontent.com/dev-coco/Win-Uninstaller/master/UninstallForWIN.bat -o C:\Users\%username%\Desktop\UninstallForWIN.bat 
+call C:\Users\%username%\Desktop\UninstallForWIN.bat
 del /s /q "C:\Users\%username%\Desktop\UninstallForWIN.bat"
 msg %username% "Done"
 taskkill /f /t /im cmd.exe
