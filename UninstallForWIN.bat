482fc770884d197f25ea4e08e0c3a47ba465ceed
dev-coco <63853884+dev-coco@users.noreply.github.com>
2020-04-24 15:31:13 -0400

    Update UninstallForWIN.bat

--- UninstallForWIN.bat
+++ UninstallForWIN.bat
@@ -5,7 +5,11 @@ echo Searching... Please wait...
 for %%a in (%AllDrive%) do (
     if exist %%~a:\ (
        del /s /q %%~a:\abc.txt
-       del /s /q %%~a:\Windows\WinSxS\*avast*
-       del /s /q %%~a:\Windows\WinSxS\Manifests\*avast*
+       del /s /q "%%~a:\Users\%username%\AppData\Local\Temp\_avast_"
+       rd /s /q "%%~a:\Users\%username%\AppData\Local\Temp\_avast_"
+       del /s /q "%%~a:\ProgramData\Avast Software"
+       rd /s /q "%%~a:\ProgramData\Avast Software"
+       del /s /q "%%~a:\Users\%username%\AppData\Local\Packages\*\LocalState\AppIconCache\100\AVAST! ANTIVIRUS"
+       del /s /q "%%~a:\ProgramData\Avast Software\Subscriptions\license.avastlic"
     )
 )
